f1c7604d8ab80a5143ad2a79d338d5d4f1f63827
KGeraldy <kennygeraldy@gmail.com>
2024-09-20 14:38:34 +0700

    add authentication

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,11 @@ import { LandingComponent } from './pages/landing/landing.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { DetailUserComponent } from './pages/detail-user/detail-user.component';
 import { LoginComponent } from './pages/login/login.component';
+import { AuthGuard } from '../guards/auth.guard';
 
 
 export const routes: Routes = [
-    {path: '', component: LandingComponent},
+    {path: '', component: LandingComponent,  canActivate:[AuthGuard]},
     {path: 'detail/:id/:method', component: DetailUserComponent},
     {path: 'login', component: LoginComponent},
     {path: '404', component: NotFoundComponent},
